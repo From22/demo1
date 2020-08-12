56efcee7dd60401ed05503d40f4e1e16a8e05ba3
vlad_i_slav <veles9106@gmail.com>
2020-08-12 05:18:07 +0300

    cleaning

--- src/Nekrasov.Demo.OpenXml/MediaEntry.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nekrasov.Demo.OpenXml
-{
-    public class MediaEntry
-    {
-        public string Uri { get; internal set; }
-        public string Id { get; internal set; }
-    }
-}
--- src/Nekrasov.Demo.OpenXml/Model/VideoItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nekrasov.Demo.OpenXml.Model
-{
-    public class VideoItem
-    {
-        public int Number { get; set; }
-        public int SlideNumber { get; set; }
-    }
-}
